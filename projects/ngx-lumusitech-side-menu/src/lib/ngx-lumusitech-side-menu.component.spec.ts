87a3758e8fda5f5b7bc65e584885fbb99f046d4f
Luciano Figueroa <lumusitech@gmail.com>
2025-02-11 19:02:32 -0300

    test: add sidemenu library tests

--- projects/ngx-lumusitech-side-menu/src/lib/ngx-lumusitech-side-menu.component.spec.ts
+++ projects/ngx-lumusitech-side-menu/src/lib/ngx-lumusitech-side-menu.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { NgxLumusitechSideMenuComponent } from './ngx-lumusitech-side-menu.component';
 
 describe('NgxLumusitechSideMenuComponent', () => {
@@ -9,6 +10,7 @@ describe('NgxLumusitechSideMenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NgxLumusitechSideMenuComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NgxLumusitechSideMenuComponent);
@@ -18,5 +20,37 @@ describe('NgxLumusitechSideMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSignIn when the logout button is clicked', () => {
+    spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+
+    fixture.detectChanges();
+
+    const loginButton = fixture.nativeElement.querySelector(
+      '[data-login]'
+    ) as HTMLButtonElement;
+
+    expect(loginButton).toBeTruthy();
+
+    loginButton.click();
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+  });
+
+  it('should call onSignOut when the logout button is clicked', () => {
+    spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+
+    fixture.detectChanges();
+
+    const logoutButton = fixture.nativeElement.querySelector(
+      '[data-logout]'
+    ) as HTMLButtonElement;
+
+    expect(logoutButton).toBeTruthy();
+
+    logoutButton.click();
+    expect(component.onSignOut.emit).toHaveBeenCalled();
   });
 });
