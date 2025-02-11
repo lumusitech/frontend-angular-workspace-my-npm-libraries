4074921ecd10693f5c0551a701ae688ba46c549c
Luciano Figueroa <lumusitech@gmail.com>
2025-02-11 18:11:57 -0300

    feat: add base application that tests our libraries

---
+++ projects/lumusitech-testbed-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgxLumusitechSideMenuComponent } from 'ngx-lumusitech-side-menu';
+
+@Component({
+  selector: 'app-root',
+  imports: [NgxLumusitechSideMenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'lumusitech-testbed-app';
+}
